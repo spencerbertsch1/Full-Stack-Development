d5cbec8f565e485bd2e04ae61687bbebcc4b6a38
Spencer Bertsch <spencerbertsch99@gmail.com>
2019-07-23 16:28:08 -0400

    Add files via upload

---
+++ OOP - Python Background/NN_Linear/Main.py
@@ -0,0 +1,47 @@
+# Main - Pytorch optimization experiments
+
+#Imports
+import torch
+from torch.autograd import Variable
+import Network_Class
+import numpy as np
+
+x_data = Variable(torch.Tensor([[1.0], [2.0], [3.0]]))
+y_data = Variable(torch.Tensor([[2.0], [4.0], [6.0]]))
+
+# our model - reference the class file by using "network class.Model()"
+model = Network_Class.Model()
+print("Model Instantiated", model)
+
+# Construct our loss function and an Optimizer. The call to model.parameters()
+# in the SGD constructor will contain the learnable parameters of the two
+# nn.Linear modules which are members of the model.
+criterion = torch.nn.MSELoss(size_average=False)
+optimizer = torch.optim.SGD(model.parameters(), lr=0.01)
+print("Criterion: MSE")
+print("Optimizer: SGD")
+
+# Training loop
+for epoch in range(500):
+        # Forward pass: Compute predicted y by passing x to the model
+    y_pred = model(x_data)
+
+    # Compute and print loss
+    loss = criterion(y_pred, y_data)
+    #print(epoch, loss)
+
+    # Zero gradients, perform a backward pass, and update the weights.
+    optimizer.zero_grad()
+    loss.backward()
+    optimizer.step()
+
+# After training
+#Declare a new input for testing - network should double its inputs, so the output should be 8
+hour_var = Variable(torch.Tensor([[4.0], [10.0]]))
+#We can look at the second prediction by indexing into it using the [1][0] below
+y_pred = (model(hour_var).data[1][0])
+
+print("Y prediction:", y_pred)
+
+pred_loss = criterion(y_pred, hour_var)
+print("Prediction Loss:", pred_loss)
---
+++ OOP - Python Background/NN_Linear/Network_Class.py
@@ -0,0 +1,27 @@
+#Imports
+import torch
+import torch.nn as nn
+
+"""
+This class acts as a linear regression model which uses the self.linear
+transformation function (math:`y = xA^T + b) within the forward function
+"""
+
+class Model(torch.nn.Module):
+
+    def __init__(self):
+        """
+        In the constructor we instantiate two nn.Linear module
+        """
+        super(Model, self).__init__()
+        self.linear = torch.nn.Linear(1, 1)  # One in and one out
+
+    def forward(self, x):
+        """
+        In the forward function we accept a Variable of input data and we must return
+        a Variable of output data. We can use Modules defined in the constructor as
+        well as arbitrary operators on Variables.
+        """
+        y_pred = self.linear(x)
+        return y_pred
+
