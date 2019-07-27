f275ac385c94c0efb63e86e50581af92d1323da8
Spencer R. Bertsch <spencerbertsch99@gmail.com>
2019-07-27 16:50:15 -0400

    Define EchoServer and test

--- Web_From_Python/Lesson-2/1_EchoServer/EchoServer.py
+++ Web_From_Python/Lesson-2/1_EchoServer/EchoServer.py
@@ -18,8 +18,7 @@
 
 from http.server import HTTPServer, BaseHTTPRequestHandler
 
-
-class HelloHandler(BaseHTTPRequestHandler):
+class EchoHandler(BaseHTTPRequestHandler):
     def do_GET(self):
         # First, send a 200 OK response.
         self.send_response(200)
@@ -28,10 +27,13 @@ class HelloHandler(BaseHTTPRequestHandler):
         self.send_header('Content-type', 'text/plain; charset=utf-8')
         self.end_headers()
 
+        #Define the query path!
+        path_ = self.path[1:]
+
         # Now, write the response body.
-        self.wfile.write("Hello, HTTP!\n".encode())
+        self.wfile.write(path_.encode())
 
 if __name__ == '__main__':
     server_address = ('', 8000)  # Serve on all addresses, port 8000.
-    httpd = HTTPServer(server_address, HelloHandler)
+    httpd = HTTPServer(server_address, EchoHandler)
     httpd.serve_forever()
