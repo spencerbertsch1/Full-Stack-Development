71eddcd0885aa9028f92e5188194be402067428c
Spencer R. Bertsch <spencerbertsch99@gmail.com>
2019-07-28 12:25:46 -0400

    Add do_GET method to return the form

--- Web_From_Python/Lesson-2/4_MessageboardPartTwo/MessageboardPartTwo.py
+++ Web_From_Python/Lesson-2/4_MessageboardPartTwo/MessageboardPartTwo.py
@@ -17,23 +17,48 @@
 from http.server import HTTPServer, BaseHTTPRequestHandler
 from urllib.parse import parse_qs
 
+form = '''<!DOCTYPE html>
+  <title>Message Board</title>
+  <form method="POST" action="http://localhost:8000/">
+    <textarea name="message"></textarea>
+    <br>
+    <button type="submit">Post it!</button>
+  </form>
+'''
 
 class MessageHandler(BaseHTTPRequestHandler):
     def do_POST(self):
-        # How long was the message?
-        length = int(self.headers.get('Content-length', 0))
+        # 1. How long was the message? (Use the Content-Length header.)
+        message_length = self.headers.get('Content-length', 0) #<-- Find length of message
+        length = int(message_length) #<-- Convert that string to an int
 
-        # Read the correct amount of data from the request.
-        data = self.rfile.read(length).decode()
 
-        # Extract the "message" field from the request data.
+        # 2. Read the correct amount of data from the request.
+        request_data = self.rfile.read(length) #<-- Uses the self.rfile object and the read() method to read the request body
+        data = request_data.decode() #<-- We now need to decode the result
+
+        # 3. Extract the "message" field from the request data.
         message = parse_qs(data)["message"][0]
+
+        print("Message Data:", data)
+        print("Parsed message data:", message)
 
         # Send the "message" field back as the response.
         self.send_response(200)
         self.send_header('Content-type', 'text/plain; charset=utf-8')
         self.end_headers()
         self.wfile.write(message.encode())
+
+    def do_GET(self):
+        #Part 1 - sends send_response
+        self.send_response(200)
+
+        #Part 2 - send end_headers
+        self.send_header('Content-type', 'text/html; charset=utf-8')
+        self.end_headers()
+
+        #Part 3 - write the response body
+        self.wfile.write(form.encode())
 
 if __name__ == '__main__':
     server_address = ('', 8000)
