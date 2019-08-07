074bfafe0de1fdbc1a6c2f1b1b85090a2eaa336a
Spencer R. Bertsch <spencerbertsch99@gmail.com>
2019-08-07 12:47:35 -0400

    add section on joining 3 tables at once

--- Databases with SQL and Python/simple_join.sql
+++ Databases with SQL and Python/simple_join.sql
@@ -22,6 +22,27 @@ where animals.species = diet.species
 and diet.food = 'fish'
 limit 100
 
+-- List all the taxonomic orders, using their common names, sorted by the
+-- number of animals of that order that the zoo has.
+--
+-- The animals table has (name, species, birthdate) for each individual.
+-- The taxonomy table has (name, species, genus, family, t_order) for each species.
+-- The ordernames table has (t_order, name) for each order.
+--
+-- Be careful:  Each of these tables has a column "name", but they don't
+-- have the same meaning!  animals.name is an animal's individual name.
+-- taxonomy.name is a species' common name (like 'brown bear').
+-- And ordernames.name is the common name of an order (like 'Carnivores').
+
+-- We can join more than two tables to get a result!
+
+select ordernames.name, count(*) as num
+  from animals, taxonomy, ordernames
+  where animals.species = taxonomy.name
+    and taxonomy.t_order = ordernames.t_order
+  group by ordernames.name
+  order by num desc
+
 -----------------------------------
 -- Practice with where vs. having --
 -- where comes before the aggregation, and having comes after! --
