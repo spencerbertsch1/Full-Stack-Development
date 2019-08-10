3e8ad861e4b76fe5d2faeb83f05c6633af41ef17
Spencer R. Bertsch <spencerbertsch99@gmail.com>
2019-08-09 20:07:24 -0400

    add nested query example in python

---
+++ Databases with SQL and Python/DB-SQL nested query example.py
@@ -0,0 +1,16 @@
+# Find the players whose weight is less than the average.
+# 
+# The function below performs two database queries in order to find the right players.
+# Refactor this code so that it performs only one query.
+#
+
+def lightweights(cursor):
+    """Returns a list of the players in the db whose weight is less than the average."""
+    cursor.execute("select avg(weight) as av from players;")
+    av = cursor.fetchall()[0][0]  # first column of first (and only) row
+    cursor.execute(select name, weight 
+    				from players,
+                        (select avg(weight) as avg_weight 
+                        from players) as light_players
+                    where weight < avg_weight);
+    return cursor.fetchall()
